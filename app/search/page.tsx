cc9dbaeeef57dc47335c60e91ae2d1c300470725
Nouran Mostafa <132231010+nouran25@users.noreply.github.com>
2025-10-30 17:19:55 +0300

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const revalidate = 0; // disable static generation cache
 
 import React, { useState } from "react";
 
@@ -198,3 +200,4 @@ export default function SearchPage() {
     </div>
   );
 }
+
