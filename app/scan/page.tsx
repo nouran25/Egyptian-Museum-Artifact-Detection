a743efe031a560208466e359965eb3a254a4bde6
Nouran Mostafa <132231010+nouran25@users.noreply.github.com>
2025-10-30 17:39:54 +0300

    Update page.tsx

--- app/scan/page.tsx
+++ app/scan/page.tsx
@@ -1,6 +1,5 @@
+import "../config";
 "use client";
-export const dynamic = "force-dynamic";
-export const revalidate = 0; // disable static generation cache
 
 import React, { useState } from "react";
 
@@ -241,4 +240,5 @@ export default function ScanPage() {
     </div>
   );
 }
+
 
