85fbd357c8d723b1e4e36404fe25838ae8a925db
Nouran Mostafa <132231010+nouran25@users.noreply.github.com>
2025-10-30 17:30:06 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    serverActions: false,
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
